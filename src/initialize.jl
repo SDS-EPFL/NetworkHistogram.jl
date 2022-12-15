ae70e378148dda4d1e16116ecabe54c6d120f23d
Jake Grainger <57754016+JakeGrainger@users.noreply.github.com>
2022-12-15 13:47:18 +0100

    Add method for initial construction of Assignment

--- src/initialize.jl
+++ src/initialize.jl
@@ -8,10 +8,31 @@ struct Assignment
     estimated_theta::Matrix{Float64}
 
     likelihood::Float64
-    function Assignment(A, h)
+    function Assignment(A, node_labels)
+        
+        number_groups = length(unique(node_labels))
+        number_nodes = length(node_labels)
+        estimated_theta = zeros(Float64, number_nodes, number_groups)
+        counts = zeros(Int64, number_groups, number_groups)
+        realized = zeros(Int64, number_groups, number_groups)
+        size_groups = [count(==(element), node_labels) for element in unique(node_labels)]
 
-        # need to fill this in
+        @inbounds @simd for k in 1:number_groups
+            for l in k:number_groups
+                realized[k, l] = sum(adjacency_matrix[node_labels.==k, node_labels.==l])
+                realized[l, k] = realized[k, l]
+                counts[k, l] = size_groups[k] * size_groups[l]
+                counts[l, k] = counts[k, l]
+            end
+        end
 
+        @inbounds @simd for k in 1:number_groups
+            counts[k, k] = size_groups[k] * (size_groups[k] - 1) / 2
+            realized[k, k] = sum(adjacency_matrix[node_labels.==k, node_labels.==k]) / 2
+        end
+
+        estimated_theta = realized ./ counts
+      
         new(
             number_nodes,
             number_groups,
@@ -38,7 +59,8 @@ struct Assignment
 end
 
 function initialize(A, h)
-    old_store = Assignment(A, h)
+    node_labels = initialise_node_labels(A, h)
+    old_store = Assignment(A, node_labels)
     new_store = deepcopy(oldstore)
     history = MVHistory([:likelihood => QHistory(Float64), :best_likelihood => QHistory(Float64), :proposal_likelihood => QHistory(Float64)])
     return old_store, new_store, history
