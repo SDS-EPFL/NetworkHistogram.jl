4b426198b581f2f34348bea3ce1b475c469860cd
JakeGrainger <j.p.grainger2@outlook.com>
2023-02-03 13:29:38 +0100

    Catch group numbering size 1 errors.

--- src/group_numbering.jl
+++ src/group_numbering.jl
@@ -7,13 +7,21 @@ struct GroupSize{T} <: AbstractVector{Int}
         GroupSize(number_nodes, standard_group)
     end
     function GroupSize(number_nodes, standard_group::Int)
-        @assert 0 < standard_group < number_nodes
+        @assert 1 < standard_group < number_nodes
         number_groups = number_nodes ÷ standard_group
         if number_groups * standard_group == number_nodes
             new{Int}(standard_group, number_groups)
         else
-            extra_group = number_nodes - number_groups * standard_group
-            new{Tuple{Int,Int}}((standard_group, extra_group), number_groups+1)
+            remainder_group = number_nodes - number_groups * standard_group
+            if remainder_group == 1
+                @warn "h has to be changes as only one node in remainder group"
+                standard_group -= 1
+                remainder_group = number_groups # because equal to 1+number_groups-1 because we take 1 from each standard group, and there are number_groups-1 of them (and we had 1 before in remainder)
+                if standard_group == 1
+                    error("Standard group size now 1, please choose a new h value.")
+                end
+            end
+            new{Tuple{Int,Int}}((standard_group, remainder_group), number_groups+1)
         end
     end
 end
