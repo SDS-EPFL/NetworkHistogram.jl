69027be01e81b83f94e31d76215d4f0eb9973c70
dufourc1 <dufourc115@gmail.com>
2023-02-04 07:01:00 +0100

    add test for proposal

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 using JLD
 
-@testset "NetworkHistogram.jl" begin include("pipeline_test.jl") end
+@testset "NetworkHistogram.jl" begin
+    include("pipeline_test.jl")
+    include("proposal_test.jl")
+end
