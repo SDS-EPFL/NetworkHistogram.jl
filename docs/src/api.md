d50033c2639bdc24f268c81c76c48e9f57d2368b
dufourc1 <dufourc115@gmail.com>
2023-02-04 08:00:37 +0100

    Add docs for proposal

--- docs/src/api.md
+++ docs/src/api.md
@@ -3,7 +3,17 @@ Pages = ["api.md"]
 Depth = 1
 ```
 
+# Assignment 
+
 ```@autodocs
 Modules = [NetworkHistogram]
 Pages   = ["assignment.jl"]
+```
+
+
+# Proposal
+
+```@autodocs
+Modules = [NetworkHistogram]
+Pages   = ["proposal.jl"]
 ```
--- src/proposal.jl
+++ src/proposal.jl
@@ -1,3 +1,16 @@
+"""Functions to create and evaluate possible labels update."""
+
+"""
+    create_proposal!(history::MVHistory, iteration::Int, proposal::Assignment,
+                          current::Assignment, A, swap_rule)
+
+Create a new proposal by swapping the labels of two nodes. The new assignment is stored in
+`proposal`. The swap is selected using the `swap_rule` function. The likelihood of the new
+proposal is stored in the history.
+
+!!! warning
+    The `proposal` assignment is modified in place to avoid unnecessary memory allocation.
+"""
 function create_proposal!(history::MVHistory, iteration::Int, proposal::Assignment,
                           current::Assignment, A, swap_rule)
     swap = select_swap(current, A, swap_rule)
@@ -6,6 +19,12 @@ function create_proposal!(history::MVHistory, iteration::Int, proposal::Assignme
     return proposal
 end
 
+"""
+    make_proposal!(proposal::Assignment, current::Assignment, swap::Tuple{Int, Int}, A)
+
+From the current assignment, create a new assignment by swapping the labels of the nodes
+specified in `swap`. The new assignment is stored in `proposal`.
+"""
 function make_proposal!(proposal::Assignment, current::Assignment, swap::Tuple{Int, Int}, A)
     # copy current in proposal
     deepcopy!(proposal, current)
@@ -14,19 +33,36 @@ function make_proposal!(proposal::Assignment, current::Assignment, swap::Tuple{I
     # update realized, estimated_theta
     update_observed!(proposal, swap, A)
     # update ll
-    updateLL!(proposal, current, swap)
+    updateLL!(proposal)
 end
 
+"""
+    update_labels!(proposal::Assignment, swap::Tuple{Int, Int}, current::Assignment)
+
+Update the labels of the nodes specified in `swap` in the `proposal` assignment.
+"""
 function update_labels!(proposal::Assignment, swap::Tuple{Int, Int}, current::Assignment)
     proposal.node_labels[swap[1]] = current.node_labels[swap[2]]
     proposal.node_labels[swap[2]] = current.node_labels[swap[1]]
 end
 
-function updateLL!(proposal::Assignment, current::Assignment, swap::Tuple{Int, Int})
+"""
+    updateLL!(proposal::Assignment)
+
+Update the likelihood of the `proposal` assignment based on its observed and estimated
+attributes.
+"""
+function updateLL!(proposal::Assignment)
     # O(G^2) where G is the number of groups
     proposal.likelihood[1] = NetworkHistogram.compute_log_likelihood(proposal)
 end
 
+"""
+    update_observed!(proposal::Assignment, swap::Tuple{Int, Int}, A)
+
+Update the observed and estimated attributes of the `proposal` assignment based on the
+swap of the nodes specified in `swap`.
+"""
 function update_observed!(proposal::Assignment, swap::Tuple{Int, Int}, A)
     group_updated = [proposal.node_labels[swap[1]], proposal.node_labels[swap[2]]]
 
