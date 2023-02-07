5f50571181db1e37aa67530f07e87e00024d27cf
dufourc1 <dufourc115@gmail.com>
2023-02-07 19:20:51 +0100

    add docs

--- docs/src/api.md
+++ docs/src/api.md
@@ -7,7 +7,7 @@ Depth = 1
 
 ```@autodocs
 Modules = [NetworkHistogram]
-Pages   = ["assignment.jl"]
+Pages   = ["assignment.jl", "group_numbering.jl"]
 ```
 
 
@@ -16,4 +16,18 @@ Pages   = ["assignment.jl"]
 ```@autodocs
 Modules = [NetworkHistogram]
 Pages   = ["proposal.jl"]
+```
+
+
+# Starting node labels
+
+!!! note
+    The groups will be of size `floor(h * n)` where `n` is the number of nodes if `h` is a
+    float. If `h` is an integer, the groups will be of size `h`. The last group may be
+    smaller if `n` is not perfectely divisible by the group size.
+
+
+```@autodocs
+Modules = [NetworkHistogram]
+Pages   = ["starting_assignment_rule.jl"]
 ```
--- src/group_numbering.jl
+++ src/group_numbering.jl
@@ -1,11 +1,16 @@
+"""
+Array-like storage for the number of nodes in each group.
+"""
 struct GroupSize{T} <: AbstractVector{Int}
     group_number::T
     number_groups::Int
+
     function GroupSize(number_nodes, h::Real)
         @assert 0 < h < 1
         standard_group = floor(Int, number_nodes * h)
         GroupSize(number_nodes, standard_group)
     end
+
     function GroupSize(number_nodes, standard_group::Int)
         @assert 1 < standard_group < number_nodes
         number_groups = number_nodes ÷ standard_group # number of standard groups!
@@ -25,11 +30,13 @@ struct GroupSize{T} <: AbstractVector{Int}
         end
     end
 end
+
 Base.size(g::GroupSize) = (g.number_groups,)
 Base.@propagate_inbounds function Base.getindex(g::GroupSize{Int}, i::Int)
     @boundscheck checkbounds(g, i)
     return g.group_number
 end
+
 Base.@propagate_inbounds function Base.getindex(g::GroupSize{Tuple{Int, Int}}, i::Int)
     @boundscheck checkbounds(g, i)
     return i < length(g) ? g.group_number[1] : g.group_number[2]
