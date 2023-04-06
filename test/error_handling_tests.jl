45c61674254cf43567f14a0880f5394f1c02871b
dufourc1 <dufourc115@gmail.com>
2023-04-06 13:02:47 +0200

    linting

--- test/error_handling_tests.jl
+++ test/error_handling_tests.jl
@@ -15,9 +15,8 @@
         end
         @test_throws AssertionError graphhist(["0" "1"; "1" "0"], h = 2)
     end
-    @testset "maxitr" begin
-        @test_throws AssertionError graphhist([0 1; 1 0], h = 2, maxitr = -1)
-    end
+    @testset "maxitr" begin @test_throws AssertionError graphhist([0 1; 1 0], h = 2,
+                                                                  maxitr = -1) end
     @testset "h" begin for h in (3, -1, 1.1, -0.1)
         @test_throws AssertionError graphhist([0 1; 1 0], h = h)
     end end
