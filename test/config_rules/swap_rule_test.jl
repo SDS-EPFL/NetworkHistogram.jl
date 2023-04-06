c0d5e663aac680d7463a039479dd4ebe3976be1c
JakeGrainger <j.p.grainger2@outlook.com>
2023-04-06 13:59:31 +0200

    Add swap rule tests.

--- test/config_rules/swap_rule_test.jl
+++ test/config_rules/swap_rule_test.jl
@@ -1,1 +1,7 @@
-@testset "swap rule" begin end
+import NetworkHistogram: select_swap
+@testset "swap rule" begin
+    A, node_labels, group_size, assignment = make_simple_example()
+    x = select_swap(assignment, A, RandomNodeSwap())
+    @test x isa Tuple{Int,Int}
+    @test all(1 .≤ x .≤ size(A,1))
+end
