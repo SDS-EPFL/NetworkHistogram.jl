14c8bbc5b7642e2cf23041e1331b92a7b75dc37a
JakeGrainger <j.p.grainger2@outlook.com>
2023-04-06 11:52:36 +0200

    Change load order.

--- src/NetworkHistogram.jl
+++ src/NetworkHistogram.jl
@@ -8,14 +8,14 @@ export OrderedStart, RandomStart, EigenStart
 include("group_numbering.jl")
 include("assignment.jl")
 include("history.jl")
-include("optimize.jl")
-include("histogram.jl")
 
 include("config_rules/starting_assignment_rule.jl")
 include("config_rules/swap_rule.jl")
 include("config_rules/accept_rule.jl")
 include("config_rules/stop_rule.jl")
 
+include("optimize.jl")
+include("histogram.jl")
 include("proposal.jl")
 
 include("utils.jl")
