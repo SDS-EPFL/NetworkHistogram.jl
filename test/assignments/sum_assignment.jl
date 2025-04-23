ad1448d5993f4cfdfa85aaa432e4f292e4f95fc7
dufourc1 <dufourc115@gmail.com>
2025-04-23 17:45:47 +0200

    typo

--- test/assignments/sum_assignment.jl
+++ test/assignments/sum_assignment.jl
@@ -5,6 +5,5 @@ using Random
 
 @testset "test sum assignment" begin
     using Distributions, LinearAlgebra, SparseArrays
-    @test_broken 1 == 2
     @error "This test is not implemented yet"
 end
