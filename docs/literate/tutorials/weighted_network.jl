a3ce9294e17127915d09fff9f1a4f75f9bcca372
dufourc1 <dufourc115@gmail.com>
2025-10-27 20:52:15 +0100

    weighted seems to work

--- docs/literate/tutorials/weighted_network.jl
+++ docs/literate/tutorials/weighted_network.jl
@@ -22,35 +22,44 @@ let
     fig
 end
 
-n = 5000
-k = 15
 A = sample_graph(graphon, n) .* Symmetric(rand(Bernoulli(0.9), n, n));
 oracle_latents = ordered_start_labels(n, k);
 starting_labels = copy(oracle_latents);
-# shuffle!(starting_labels);
 p_shuffle = 1 - 1.5 / k
 @info "Shuffling $(p_shuffle*100)% labels for starting point"
 indices_to_shuffle = sample(1:n, floor(Int, n * p_shuffle), replace = false);
 starting_labels[indices_to_shuffle] .= shuffle(starting_labels[indices_to_shuffle]);
 @assert starting_labels != oracle_latents
 
-res, res_cat, A_cat = NetworkHistogram.nethist_continuous_edges(A,
-    starting_labels, GreedyParams(
-        1_000_000,
-        RandomGroupSwap(),
-        Strict(),
-        PreviousBestValue(10_000, Inf, :min),
-        true # progress bar
-    );
-    num_bins_ = 10, lower_bound = eps(), upper_bound = 1);
+convertor = NetworkHistogram.UnitIntervalConvertor(10)
 
-latents = range(0, 1; length = n);
+max_iter = 1_000_000
+stalled_iters = 5_000
 
-ssm_test = SSM(res.model, k)
+data = convertor.(A)
+es_new = NetworkHistogram.GreedySuffStats(
+    data, initial_labels, num_categories = num_bins(convertor),
+    type_suff_stats = :categorical,
+    max_iter = max_iter,
+    swap_rule = NetworkHistogram.RandomGroupSwap(),
+    stop_rule = NetworkHistogram.PreviousBestValue(stalled_iters, Inf, :min),
+    progress = true
+);
+node_labels_es_new, parameters = NetworkHistogram.estimate!(
+    es_new, data, initial_labels; iter_progress = 10_000)
+
+model_es_new = NetworkHistogram.DecoratedSBM(to_distribution.(convertor, parameters),
+    counts(node_labels_es_new) ./ length(node_labels_es_new));
+
+res_new = NetworkHistogram.NethistResult(node_labels_es_new, model_es_new);
+NetworkHistogram.align_res_true_latents!(res_new, oracle_labels);
+
+##
+ssm_test = SSM(res_new.model, k)
 
 shape_range = 1:min(30, k * (k + 1) ÷ 2 - 1)
 ssm_estimated, criterion_values = Graphons.estimate_ssm(
-    res_cat.model, A_cat, latents, shape_range)
+    res_new.model, A_cat, latents, shape_range)
 
 Mke.lines(shape_range, criterion_values)
 
@@ -60,4 +69,4 @@ kr = kneedle(shape_range, criterion_values, "convex_dec", 1, scan_type = :smooth
 #  Let's extract the optimal number of shapes using the Kneedle algorithm:
 
 k_knee = knees(kr)[1]
-ssm_knee = SSM(res.model, k_knee)
+ssm_knee = SSM(res_new.model, k_knee)
