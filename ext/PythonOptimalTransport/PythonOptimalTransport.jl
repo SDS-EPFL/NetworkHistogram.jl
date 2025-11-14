bdf54fd554f28dc31725c8b17d9ae51e0d2e3b1a
dufourc1 <dufourc115@gmail.com>
2025-11-14 11:01:51 +0100

    add docs to pot function

--- ext/PythonOptimalTransport/PythonOptimalTransport.jl
+++ ext/PythonOptimalTransport/PythonOptimalTransport.jl
@@ -15,6 +15,12 @@ end
 
 jl_to_np(mat) = Py(mat).to_numpy()
 
+"""
+Get the permutation aligning source and target matrices using optimal transport.
+
+This function converts a gromov-wasserstein plan into a permutation by taking the argmax
+along the rows.
+"""
 function get_perm_alignment(src, target)
     plan = ot[].gromov.gromov_wasserstein(
         C2 = jl_to_np(src), C1 = jl_to_np(target))
@@ -24,6 +30,12 @@ function get_perm_alignment(src, target)
     return perm
 end
 
+"""
+Align the source and target matrices using optimal transport.
+
+# See Also
+- [`get_perm_alignment`](@ref).
+"""
 function align_matrices(src, target)
     perm = get_perm_alignment(src, target)
     return src[perm, perm]
